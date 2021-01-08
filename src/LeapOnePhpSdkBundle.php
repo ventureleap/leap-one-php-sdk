826e60efd2e00151637ea0b677683e7cafed12ee
Niklas <kingpfogel@github.com>
2021-01-08 11:52:13 +0100

    test

--- src/LeapOnePhpSdkBundle.php
+++ src/LeapOnePhpSdkBundle.php
@@ -4,12 +4,18 @@ namespace VentureLeap\LeapOnePhpSdk;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use VentureLeap\LeapOnePhpSdk\DependencyInjection\Compiler\AddProviderCompilerPass;
+use VentureLeap\LeapOnePhpSdk\DependencyInjection\Compiler\CustomConfigurationCompilerPass;
+use VentureLeap\LeapOnePhpSdk\DependencyInjection\Compiler\StorageConfigurationCompilerPass;
 use VentureLeap\LeapOnePhpSdk\DependencyInjection\Compiler\UserProviderCompilerPass;
 
 class LeapOnePhpSdkBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
+        $container->addCompilerPass(new AddProviderCompilerPass());
+        $container->addCompilerPass(new StorageConfigurationCompilerPass());
+        $container->addCompilerPass(new CustomConfigurationCompilerPass());
         $container->addCompilerPass(new UserProviderCompilerPass());
     }
 
